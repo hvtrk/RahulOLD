a0b63b8a46a1b1bc40874712e03b732936046ecc
Rahul Kumar <dr.rahulk95@gmail.com>
2019-10-17 19:45:04 +0530

    fix

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home' , component: LandingPageComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '' , redirectTo: 'home', pathMatch: 'full' },
+  { path: '**' , redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from './shared/shared.module';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    LandingPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    SharedModule
   ],
+  // schemas: [
+  //   CUSTOM_ELEMENTS_SCHEMA,
+  // ],
   providers: [],
   bootstrap: [AppComponent]
 })
